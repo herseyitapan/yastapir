43c80109eba63b142e14a7ae7ddd8aaa147607a6
Xuytaparsan <105146418+Xuytaparsan@users.noreply.github.com>
2022-05-20 13:14:37 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	token: "5342012614:AAErZfKH5LjJe0aiLgJJhTpgsnpz6F3fXFI",
+	botId: 5342012614, //first numbers of BOT_TOKEN
+	rounds: 20, //rounds amount
+	waitDelay: 12000, //1 round duration
+	timerSteps: 4, //timer indicators
+}
